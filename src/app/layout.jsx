195c4d79da5aed2edabd398cac69d959c25e30ef
aish13090 <saishvarya24ece@srishakthi.ac.in>
2024-05-10 10:51:34 +0530

    hi

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import Header from '@components/common/header/header';
 import Footer from '@components/common/footer/Footer';
 import Banner from '@public/assets/Images/landing-page/banner-image.png';
 import Head from 'next/head';
-import Favi from '@public/assets/Images/Favicon.png';
+import Favi from '../public/assets/Images/Favicon.ico';
 
 export const metadata = {
  
@@ -16,16 +16,16 @@ export const metadata = {
     description: 'Learn UI/UX designing, artificial intelligence, and digital marketing with our interactive courses and accelerate your career with expert guidance',
     images:Banner
   },
-  // icons: Favi
+  icons: '../public/assets/Images/Favicon.ico'
   
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-     
-      {/* <link rel="icon" href={Favi} type="image/x-icon" /> */}
-     
+     <Head>
+      <link rel="icon" href={Favi} type="image/x-icon" />
+      </Head>
       <body>
         <main className="app">
           <Header />
