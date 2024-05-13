4fddc0f0db3be7f8089272537347e2f1fd28307a
thivagarradhakrishnan <ajithivagar@gmail.com>
2024-05-13 22:54:08 +0530

    hii

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    mySecret: 'secret',
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    staticFolder: '/static',
-  },
-  server: {
-    port: 4000, // Change this to the port you want to use
-    host: '0.0.0.0', // Bind to all network interfaces
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
