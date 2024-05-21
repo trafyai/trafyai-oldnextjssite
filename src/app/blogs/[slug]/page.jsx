59559ebb44220676139682fa8664ed3dce2cdb28
aish13090 <saishvarya24ece@srishakthi.ac.in>
2024-05-21 16:26:51 +0530

    hi

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -1,45 +1,38 @@
-import React from 'react'
-import BlogSingleData from '@api/blog/BlogSingleData';
-import BlogPage from '@components/blog-page/blog-single/BlogSingle'
-import Open from './opengraph-image'
-import Image from './opengraph-image';
+import React from 'react';
+import BlogSingleData from '@api/blog/BlogSingleData'; // Assuming this fetches blog data
+import BlogPage from '@components/blog-page/blog-single/BlogSingle'; // Blog post component
 
 import Head from 'next/head';
 export async function generateMetadata({ params, searchParams }, parent) {
-  // read route params
-  const id = params.slug
- 
-  
-  const product = BlogSingleData.find(blog =>blog.id === id);
-
+  const id= params.slug;
+  const product = BlogSingleData.find(blog => blog.id === id);
 
   return {
     title: product.title,
-    description:product.metaDescription,
-    metadataBase: new URL(`https://trafyai.com/blogs/${product.id}`),
-    type:"article",
-    image:[Open],
+    description: product.metaDescription,
+    metadataBase: `https://trafyai.com/blogs/${product.id}`,
+    type: 'article',
+    // image: product.metaImage,
     openGraph: {
       title: product.title,
-      description:product.metaDescription
+      description: product.metaDescription,
+         
     },
   }
 }
 
-const page = ({params}) => {
-  const {slug} = params;
-  const BlogData = BlogSingleData.find(blog =>blog.id === slug);
+const Page = ({ params }) => {
+  const id = params.slug;
+  const BlogData = BlogSingleData.find(blog => blog.id === id);
 
   return (
     <div>
-     <Head>
-     <meta property="og:image" content={Image} />
-     </Head>
-     {/* <Open/> */}
-     
+       {/* <Head>
+     <meta property="og:image" content={BlogData.metaImage} />
+     </Head> */}
       <BlogPage {...BlogData} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
