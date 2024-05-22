149cff1686628c5b00a374ba53ee59dabc85ed76
aish13090 <saishvarya24ece@srishakthi.ac.in>
2024-05-22 16:49:24 +0530

    g

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import BlogSingleData from '@api/blog/BlogSingleData'; // Assuming this fetches blog data
 import BlogPage from '@components/blog-page/blog-single/BlogSingle'; // Blog post component
-
+import Image from 'next/image';
 import Head from 'next/head';
 export async function generateMetadata({ params, searchParams }, parent) {
   const id= params.slug;
@@ -27,9 +27,7 @@ const Page = ({ params }) => {
 
   return (
     <div>
-       {/* <Head>
-     <meta property="og:image" content={BlogData.metaImage} />
-     </Head> */}
+       
       <BlogPage {...BlogData} />
     </div>
   );
